4a1a58fe2b590e06e0908f73df04e17f8c51c392
mpa2 <mpa2@d6886180-81a5-43a1-86a0-6ff1f76a6444>
2016-09-06 15:12:35 +0000

    finished

--- mpa2/Challenge1-DebugMe/QuizMaster.java
+++ mpa2/Challenge1-DebugMe/QuizMaster.java
@@ -2,7 +2,7 @@
  * A program to test students' CS knowledge.
  * Watch out ! There may be deliberate bugs in some of the logic below :-)
  * @see QuizMaster-ReadMe.txt for details on how to complete this program.
- * @author insert-your-netid-here
+ * @author mpa2
  *
  */
 public class QuizMaster {
@@ -19,15 +19,15 @@ public class QuizMaster {
 		int gridComputing = TextIO.getlnInt();
 		
 		System.out.println("Which University CS Department designed and built the pioneering ILLIAC series?");
-		System.out.println("1. Illinois\n2. Wisconsin\n3. Berkeley\n");
+		System.out.print("1. Illinois\n2. Wisconsin\n3. Berkeley\n");
 		int illiac = TextIO.getlnInt();
 
-		System.out.println("Which University released \"Mosaic\" - the first multimedia cross-platform browser?");
+		System.out.println("Which University released \'Mosaic\' - the first multimedia cross-platform browser?");
 		System.out.println("(Mosaic's source code was later licensed to Microsoft and Netscape Communications)");
 		System.out.println("1. Illinois\n2. Michigan\n3. Wisconsin");
 		int mosaic = TextIO.getlnInt();
 		
-		System.out.println("True/False ? Variables have four things: a type, name, value and a memory location.");
+		System.out.println("True/False? Variables have four things: a type, name, value and a memory location.");
 		System.out.println("1. True");
 		System.out.println("2. False");
 		int varTF = TextIO.getlnInt();
@@ -36,14 +36,19 @@ public class QuizMaster {
 		/* Computer score: + 10 points for each correct answer */
 		if(gridComputing==1) score = score + 10;
 		if(illiac==1) score = score + 10;
+		if(mosaic==1) score = score + 10;
 		if(varTF==1) score = score + 10;
-		/* Michigan students (zip code 48xxx) need to get all of them correct to receive any points */
-		if(zip/1000 == 48) score =0;
 		
-		/* If an Illinois student gets all questions correct print congratulations */
-		if(score == 40)
-			System.out.println("\nCongratulations!");
+		/* Michigan students (zip code 48xxx) need to get all of them correct to receive any points */
+		if(zip/1000 == 48 && score != 40){
+			score = 0;
+		}
+		
 		System.out.print("You scored:");
 		System.out.print(score);
+		
+		if(zip/1000 == 61 && score == 40){
+			System.out.println("\nCongratulations!");
+		}
 	}
 }
