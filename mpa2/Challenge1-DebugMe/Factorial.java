553b82fbbbc577664fdbde9370f36e06c3e374ad
mpa2 <mpa2@d6886180-81a5-43a1-86a0-6ff1f76a6444>
2016-09-06 14:55:57 +0000

    Finished

--- mpa2/Challenge1-DebugMe/Factorial.java
+++ mpa2/Challenge1-DebugMe/Factorial.java
@@ -1,19 +1,31 @@
-
+import java.util.Scanner;
 /**
  * A program to calculate a factorial. The given code may contain errors. Fix the
  * given code and add additional code to calculate a factorial and pass the unit
  * tests. Hint sometimes an 'int' just 'aint big enough.
  * 
  * @see Factorial-ReadMe.txt for details on how to complete this program.
- * @author insert-your-netid-here
+ * @author mpa2
  */
 public class Factorial {
 	public static void main(String[] args) {
 		int max = 0;
-		System.out.println("Enter a number between 1 and 20 inclusive.");
-		while (max < 1 || max >= 21) {
-			max = TextIO.getlnInt();
+		long factorial = 1;
+		
+		Scanner scan = new Scanner(System.in);
+		
+		
+		while (max < 1 || max > 20) {
+			System.out.println("Enter a number between 1 and 20 inclusive.");
+			max = scan.nextInt();
 		}
-		TextIO.putln(42);
+		scan.close();
+		
+		for(int x = max; x > 0; x--){
+			factorial = factorial * x;
+		}
+		
+		System.out.println(max + "! = " + factorial);
+		
 	}
 }
